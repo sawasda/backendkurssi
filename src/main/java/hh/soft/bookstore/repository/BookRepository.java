cac847fca1b387acc4896028c187b5836686eb12
sawasda <sara.junnila@haaga-helia.fi>
2026-03-03 08:52:37 +0200

    Auto-generated Spring Data REST

--- src/main/java/hh/soft/bookstore/repository/BookRepository.java
+++ src/main/java/hh/soft/bookstore/repository/BookRepository.java
@@ -1,8 +1,15 @@
 package hh.soft.bookstore.repository;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import hh.soft.bookstore.domain.Book;
 
-public interface BookRepository extends CrudRepository <Book, Long> {
+@RepositoryRestResource(path = "books")
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+    List<Book> findByTitleContainingIgnoreCase(@Param("title") String title);
 
 }
